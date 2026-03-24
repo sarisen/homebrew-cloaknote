4000fe2d64d5343962191759a2846ec610275ec3
sarisen <serafettin@turkayazilim.com>
2026-03-24 07:02:37 +0300

    Fix CloakNote cask checksum

--- Casks/cloaknote.rb
+++ Casks/cloaknote.rb
@@ -1,6 +1,6 @@
 cask "cloaknote" do
   version "1.0.0"
-  sha256 "22a042ccc9578df77e63af6fcc9166879c2c8201939fa397aaf4832b21b37a02"
+  sha256 "b4039ae845887e7b50d7b5418e3ce18b17f0ff4f27bb1ed3ca5ca8ef6845e53a"
 
   url "https://github.com/sarisen/CloakNote/releases/download/v#{version}/CloakNote-v#{version}.dmg"
   name "CloakNote"

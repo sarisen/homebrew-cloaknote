cc0d84fffd5a49f8db7892b1438a44e8398a83ce
sarisen <serafettin@turkayazilim.com>
2026-03-24 07:07:30 +0300

    Update CloakNote cask checksum

--- Casks/cloaknote.rb
+++ Casks/cloaknote.rb
@@ -1,6 +1,6 @@
 cask "cloaknote" do
   version "1.0.0"
-  sha256 "b4039ae845887e7b50d7b5418e3ce18b17f0ff4f27bb1ed3ca5ca8ef6845e53a"
+  sha256 "22c19c793c6457b898e6e80e7e5fc476f263ef1b6f5d7d8c2cffae4e9e4cb710"
 
   url "https://github.com/sarisen/CloakNote/releases/download/v#{version}/CloakNote-v#{version}.dmg"
   name "CloakNote"
